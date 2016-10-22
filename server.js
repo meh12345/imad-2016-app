87fbc00dcfb046911acc23029f5ce934a1f1ddf7
Mehul Jain <mehuljain160@gmail.com>
2016-10-23 00:13:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+app.get('/ui/article-one',function(req,res){
+   res.send('hi article 1'); 
+});
 app.get('/ui/article-three',function(req,res){
    res.send('hi article 3'); 
     
